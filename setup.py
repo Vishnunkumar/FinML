f491f71bab8569ee1db06376e5caaf490b9afe16
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2021-08-09 23:23:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Fin_ML',         # How you named your package folder (MyLib)
   packages = ['Fin_ML'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
+  version = '0.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Deep learning for Finance and Banking sectors',   # Give a short description about your library
   author = 'Vishnu N',                   # Type in your name
   author_email = 'vishnunkumar25@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Vishnunkumar/FinML/',   # Provide either the link to your github or to your website
-  download_url ='https://github.com/Vishnunkumar/FinML/archive/refs/tags/v-8.tar.gz',    # I explain this later on
+  download_url ='https://github.com/Vishnunkumar/FinML/archive/refs/tags/v-9.tar.gz',    # I explain this later on
   keywords = ['Finance', 'Machine learning', 'NLP', 'Deep learning', 'Computer Vision'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'tensorflow==2.4.1',
