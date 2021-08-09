2053a9e5164e3cabe2726f6f1e689bd905582b06
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2021-08-09 23:26:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
           'pandas',
           'yfinance',
           'fbprophet',
-          'datetime'
+          'datetime',
+          'statsmodels'
   ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
